b3e8577d85f4fde17cd272aa01499224a6445945
Evasty <35831310+Evasty@users.noreply.github.com>
2019-08-26 11:54:23 -0500

    function looks like it might compile. ..
    (now it might actually compile (ich hoffe))

--- Talleres/4/quat.cxx
+++ Talleres/4/quat.cxx
@@ -1,20 +1,21 @@
 
-
-void ptnScaleRotate ( float tx, float ty, float sx, float sy, char tp){
+//first sketch of quat funct
+void ptnScaleRotate ( float tx, float ty, float sx, float sy, char tp){//ti traslacion en i, si escala en i,tp \in {'s','c,','t'} square circle triangle 
 	glLoadIdentity();
-	glTranslate(tx,ty,0);
-	//glRotate(angle,<normal vector>) //for the sake of operation order sample, also w/e
-	glScale(sx,sy,1);
-	switch tp{
+	glTranslatef(tx,ty,0);
+	//glRotatef(angle,<normal vector>) //for the sake of operation order sample, also w/e
+	glScalef(sx,sy,1);
+	switch (tp){
 		case 's': //square
 			DrawSquare(GL_POLYGON);
 		case 'c': //circle
-			DrawCircle(GL_POLYGON);
+			DrawCircle(GL_POLYGON, 500);
 		case 't': //triangle
-			DrawTrigangle(GL_POLYGON);
+			DrawTriangle(GL_POLYGON);
 		
 	}
 	
 
 }
+
 
